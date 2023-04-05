18499308c9fa3f32083987fca9ad5c910b1f498a
blinry <mail@blinry.org>
2023-04-05 18:25:24 +0200

    Clean up nix file

--- default.nix
+++ default.nix
@@ -1,14 +1,15 @@
+# I wanna use a nixpkgs version very close to this, if possible.
 with import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/5bca69ac34e4b9aaa233aef75396830f42b2d3d7.tar.gz") { };
 
 stdenv.mkDerivation rec {
-  name = "glx-nix";
+  name = "glx-test";
   buildInputs = [ gcc mesa xorg.libX11 ];
 
-  src = ./glx.c;
+  src = ./glx-test.c;
 
   unpackPhase = "true"; # We just have a single file as the input.
   sourceRoot = "."; # Avoid an error where this variable is undefined, but default-builder.sh tries to run "cd $sourceRoot".
-  buildPhase = "gcc -g3 -o glx $src -lGL -lX11 -lGLU ";
-  installPhase = "mkdir -p $out/bin; mv glx $out/bin";
+  buildPhase = "gcc -g3 -o glx-test $src -lGL -lX11 -lGLU ";
+  installPhase = "mkdir -p $out/bin; mv glx-test $out/bin";
   dontStrip = true;
 }
