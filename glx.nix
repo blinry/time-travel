31381970282ff94f60aa2b567de742814d42ac8c
blinry <mail@blinry.org>
2023-04-05 17:05:21 +0200

    "Downgrade" to use nixpkgs from 2008

--- glx.nix
+++ glx.nix
@@ -1,12 +1,14 @@
-with import <nixpkgs> { };
+with import /home/blinry/wip/learn-nix-again/nixpkgs { };
 
 stdenv.mkDerivation rec {
-  name = "glx nix";
-  buildInputs = [ clang libGL xorg.libX11 libGLU ];
+  name = "glx-nix";
+  buildInputs = [ gcc mesa xorg.libX11 ];
 
   src = ./glx.c;
 
-  dontUnpack = true;
-  buildPhase = "clang -o glx $src -lGL -lX11 -lGLU";
+  #dontUnpack = true;
+  unpackPhase = "true"; # We just have a single file.
+  sourceRoot = "."; # Avoid an error where this variable is undefined, and default-builder.sh tries to run "cd $sourceRoot".
+  buildPhase = "gcc -o glx $src -lGL -lX11 -lGLU";
   installPhase = "mkdir -p $out/bin; mv glx $out/bin";
 }
